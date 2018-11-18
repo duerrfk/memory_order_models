cbe6a174ce44ee8a233660c15b0988c2e3a4a875
Frank Duerr <frank.d.durr@googlemail.com>
2018-11-18 11:38:44 +0100

    Initial check-in

---
+++ README.md
@@ -0,0 +1,3 @@
+The goal of the programs contained in this repository---initially implemented for the lecture System concepts and programming at University of Stuttgart---is to demonstrate the impact of different memory order models onto the semantics of concurrent programs.
+
+As example, Dekker's algorithm for synchronizing two threads is used. Note that Dekker's algorithm might not be correct under all memory order models! It is part of the exercise to find out, which memory order models ensure a correct execution of Dekker's algorithm.
---
+++ dekker-mem_order_relaxed.cc
@@ -0,0 +1,111 @@
+/**
+ * Copyright 2018 University of Stuttgart
+ * Author: Frank Duerr (frank.duerr@ipvs.uni-stuttgart.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Implementation of Dekker's Algorithm using the relaxed memory order.
+ * The purpose of this implementation is to practically show whether 
+ * Dekker's algorithm is guaranteeing mutual exclusion with relaxed memory 
+ * order.
+ */
+
+#include <iostream>
+#include <thread>
+#include <atomic>
+
+#define LOOPCNT 10000
+
+// Global variables for synchronizing thread 1 and 2.
+volatile std::atomic_bool want1 = {false};
+volatile std::atomic_bool want2 = {false};
+volatile std::atomic_int turn = {0};
+
+volatile uint16_t x = 0;
+
+void *thread1_run()
+{
+     printf("Thread 1 starting\n");
+
+     while (true) {
+	  // Pre-protocol
+	  want1.store(true, std::memory_order_relaxed);
+	  while (want2.load(std::memory_order_relaxed)) {
+	       if (turn.load(std::memory_order_relaxed) != 0) {
+		    want1.store(false, std::memory_order_relaxed);
+		    while (turn.load(std::memory_order_relaxed) != 0);
+		    want1.store(true, std::memory_order_relaxed);
+	       }
+	  }
+	  
+	  // Critical section
+	  uint8_t *x_low = (uint8_t *) &x;
+	  *x_low = 0xff;
+	  for (int i = 0; i < LOOPCNT; i++);
+	  *x_low = 0x00;
+
+	  // Post-protocol
+	  turn.store(1, std::memory_order_relaxed);
+	  want1.store(false, std::memory_order_relaxed);
+     }
+     
+     return NULL;
+}
+
+void *thread2_run()
+{
+     printf("Thread 2 starting\n");
+
+     while (true) {
+	  // Pre-protocol
+	  want2.store(true, std::memory_order_relaxed);
+	  while (want1.load(std::memory_order_relaxed)) {
+	       if (turn.load(std::memory_order_relaxed) != 1) {
+		    want2.store(false, std::memory_order_relaxed);
+		    while (turn.load(std::memory_order_relaxed) != 1);
+		    want2.store(true, std::memory_order_relaxed);
+	       }
+	  }
+	  
+	  // Critical section
+	  uint8_t *x_high = ((uint8_t *) &x)+1;
+	  *x_high = 0xff;
+	  for (int i = 0; i < LOOPCNT; i++);
+	  *x_high = 0x00;
+
+	  // Post-protocol
+	  turn.store(0, std::memory_order_relaxed);
+	  want2.store(false, std::memory_order_relaxed);
+     }
+	  
+     return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+     std::thread t1(thread1_run);
+     std::thread t2(thread2_run);
+
+     while (true) {
+	  if (x == 0xffff) {
+	       std::cout << "Mutual exclusion violated" << std::endl;
+	       exit(-1);
+	  }
+     }
+
+     t1.join();
+     t2.join();
+}
